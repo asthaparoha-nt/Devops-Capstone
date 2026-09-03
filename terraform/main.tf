17792ac395934012619d1d51fc856a11ac2651bc
asthaparoha-nt <astha@nucleusteq.com>
2026-09-03 13:30:46 +0530

    Add Terraform infrastructure for ECR

---
+++ terraform/main.tf
@@ -0,0 +1,96 @@
+resource "aws_ecr_repository" "backend" {
+  name                 = "${var.project_name}-backend"
+  image_tag_mutability = "IMMUTABLE"//this means an image tag cannot simply be overwritten with other image
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true//scanning it for vulnerabilities
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"//encrypting using aws managed encryption
+  }
+
+  tags = {
+    Name        = "${var.project_name}-backend"
+    Project     = var.project_name
+    Environment = var.environment
+    Component   = "backend"
+    ManagedBy   = "Terraform"
+  }
+}
+# Frontend ECR Repository
+
+resource "aws_ecr_repository" "frontend" {
+  name                 = "${var.project_name}-frontend"
+  image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name        = "${var.project_name}-frontend"
+    Project     = var.project_name
+    Environment = var.environment
+    Component   = "frontend"
+    ManagedBy   = "Terraform"
+  }
+}
+
+
+# ECR Lifecycle Policies
+# Backend Lifecycle Policy
+resource "aws_ecr_lifecycle_policy" "backend" {
+  repository = aws_ecr_repository.backend.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+
+        description = "Keep only the latest 10 backend images"
+
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+# Frontend Lifecycle Policy
+resource "aws_ecr_lifecycle_policy" "frontend" {
+  repository = aws_ecr_repository.frontend.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+
+        description = "Keep only the latest 10 frontend images"
+
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
