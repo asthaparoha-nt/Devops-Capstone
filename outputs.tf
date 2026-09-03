e763d6deb48a65dcd87eedc47d12c89232c50347
asthaparoha-nt <astha@nucleusteq.com>
2026-09-03 10:34:40 +0530

    updated workflow

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "oidc_provider_arn" {
-  description = "ARN of the GitHub OIDC provider registered in this AWS account"
-  value       = local.oidc_provider_arn
-}
-
-output "github_actions_role_arn" {
-  description = "ARN to paste into your GitHub Actions workflow's role-to-assume field"
-  value       = aws_iam_role.github_actions.arn
-}
